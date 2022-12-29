87df52008037b38130da650a7213932b653f7c44
tposejank <81495861+tposejank@users.noreply.github.com>
2022-12-28 19:10:59 -0500

    some stuff + new years payload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ function gne(e) {
     return document.createElement(e);
 }
 
+function clearChildren(e) {
+    while (e.firstChild) e.removeChild(e.firstChild);
+}
+
+function calcDays(v) {
+    return v / (1000 * 3600 * 24)
+}
+
 function i() {
 
     let vid = document.getElementById("fn-video");
@@ -9,7 +17,6 @@ function i() {
     fetch(geturllang('https://fortniteapi.io/v2/battlepass?season=current', 1), {
         headers: { 'Authorization': localStorage.keyFNAPIIo }
     }).then(r => r.json()).then(r => {
-
         for (let src of r.videos) {
             let i = document.createElement("source");
             i.src = src.url;
@@ -18,11 +25,10 @@ function i() {
 
         vid.muted = true;
         vid.style.width = document.body.clientWidth + 'px';
+        window.addEventListener("resize", function() {
+            vid.style.width = document.body.clientWidth + 'px';
+        });
     }).catch(err => { console.error(err); });
-
-    window.addEventListener("resize", function() {
-        vid.style.width = document.body.clientWidth + 'px';
-    });
 
     let list = document.getElementById("nav-items");
 
@@ -53,8 +59,8 @@ function i() {
                     name: 'Reality Augments'
                 },
                 {
-                    href: 'news.html',
-                    name: 'In-game News'
+                    href: 'vehicles.html',
+                    name: 'Vehicles'
                 }
             ]
         },
@@ -86,6 +92,10 @@ function i() {
                 {
                     href: 'tournaments.html',
                     name: 'Competitive'
+                },
+                {
+                    href: 'news.html',
+                    name: 'In-game News'
                 }
             ]
         },
@@ -122,7 +132,7 @@ function i() {
         {
             name: 'About',
             alone: true,
-            href: 'https://github.com/tposejank/fnlookup/blob/main/README.md'
+            href: 'about.html'
         }
     ]
 
@@ -195,8 +205,11 @@ function i() {
         doSearch();
     });
 
-    let translatorIcon = document.createElement('img');
-    translatorIcon.src = 'assets/icons/multilingual.png';
+    let translatorIcon = document.createElement('a', 'flex', 'flex-center');
+    translatorIcon.href = 'language-picker.html';
+    translatorIcon.title = 'Language Picker';
+    translatorIcon.innerHTML = '<svg viewBox="0 0 24 24" width="22" height="22"><g><path d="M11.99,1.98C6.46,1.98,1.98,6.47,1.98,12s4.48,10.02,10.01,10.02c5.54,0,10.03-4.49,10.03-10.02S17.53,1.98,11.99,1.98z M8.86,14.5c-0.16-0.82-0.25-1.65-0.25-2.5c0-0.87,0.09-1.72,0.26-2.55h6.27c0.17,0.83,0.26,1.68,0.26,2.55 c0,0.85-0.09,1.68-0.25,2.5H8.86z M14.89,15.5c-0.54,1.89-1.52,3.64-2.89,5.15c-1.37-1.5-2.35-3.25-2.89-5.15H14.89z M9.12,8.45 c0.54-1.87,1.52-3.61,2.88-5.1c1.36,1.49,2.34,3.22,2.88,5.1H9.12z M16.15,9.45h4.5c0.24,0.81,0.37,1.66,0.37,2.55 c0,0.87-0.13,1.71-0.36,2.5h-4.51c0.15-0.82,0.24-1.65,0.24-2.5C16.39,11.13,16.3,10.28,16.15,9.45z M20.29,8.45h-4.38 c-0.53-1.97-1.47-3.81-2.83-5.4C16.33,3.45,19.04,5.56,20.29,8.45z M10.92,3.05c-1.35,1.59-2.3,3.43-2.83,5.4H3.71 C4.95,5.55,7.67,3.44,10.92,3.05z M3.35,9.45h4.5C7.7,10.28,7.61,11.13,7.61,12c0,0.85,0.09,1.68,0.24,2.5H3.34 c-0.23-0.79-0.36-1.63-0.36-2.5C2.98,11.11,3.11,10.26,3.35,9.45z M3.69,15.5h4.39c0.52,1.99,1.48,3.85,2.84,5.45 C7.65,20.56,4.92,18.42,3.69,15.5z M13.09,20.95c1.36-1.6,2.32-3.46,2.84-5.45h4.39C19.08,18.42,16.35,20.55,13.09,20.95z" stroke="white"></path></g></svg>';
+    // https://www.youtube.com
     translatorIcon.classList.add('multilingual-btn');
 
     let languages = [
@@ -266,16 +279,17 @@ function i() {
             backend: 'tr'
         },
         {
-            name: '中國人',
-            title: 'Chinese',
+            name: '中國人 (简化的) (弃用)',
+            title: 'Chinese (Simplified) (Deprecated)',
             backend: 'zh-CN'
         },
         {
-            name: '中国人',
-            title: 'Chinese (Simplified)',
+            name: '中国人 (傳統的) (棄用)',
+            title: 'Chinese (Traditional) (Deprecated)',
             backend: 'zh-Hant'
         }
     ];
+    window.uiLangList = languages;
 
     let langs = gne('div');
     langs.classList.add('nav-item');
@@ -302,6 +316,11 @@ function i() {
         context.title = language.title;
         context.setAttribute('backend-value', language.backend);
         context.classList.add('link-underline', 'pointer');
+
+        if (localStorage.requestLanguage == language.backend) {
+            context.style.textDecoration = 'underline';
+            context.style.color = 'var(--season-accent-1)'
+        }
 
         context.addEventListener('click', function() {
             switchLanguage(context.getAttribute('backend-value'));
@@ -357,23 +376,24 @@ function dateNow() {
 }
 
 function addToList() {
-    var sections = [];
-    fetch(geturllang('https://fortnite-api.com/v2/shop/br', 0)).then(response => response.json()).then(response => {
-        sections.push(response.data.daily.entries[0].section.name);
-        sections.push(response.data.featured.entries[0].section.name);
+    sections = [];
+    fetch(geturllang("https://fortniteapi.io/v2/shop", 1), {
+        headers: { 'Authorization': localStorage.keyFNAPIIo}
+    }).then(shop => shop.json()).then(data => {
+        shopItems = data.shop; // save api key requests
 
-        for (var i = 0; i < response.data.specialFeatured.entries.length; i++) {
-            var item = response.data.specialFeatured.entries[i];
-            var section = item.section.name;
-
-            if (!sections.includes(section)) 
-                sections.push(section);
+        for (let item of data.shop) {
+            if (!sections.includes(item.section.name)) {
+                sections.push(item.section.name);
+            }
         }
 
         for (let sec of sections) {
             var ob = document.createElement('option');
             ob.innerHTML = sec;
             document.getElementById("shop-section-dropdown").append(ob);
+
+            if (sec == sections[sections.length -1]) iS()
         }
     });
 }
@@ -391,145 +411,191 @@ function marqueeCheck(obj) {
 }
 
 function iS() {
-    fetch(geturllang('https://fortnite-api.com/v2/shop/br', 0)).then(response => response.json()).then(response => {
-        var dropdown_list = document.getElementById("shop-section-dropdown");
-        var section = dropdown_list.value;
+    var section = document.getElementById("shop-section-dropdown").value;
 
-        var section_data;
+    var shop_row = document.getElementById("item-shop-section");
+    clearChildren(shop_row);
 
-        if (response.status !== 200) {
-            let eTitle = document.createElement('h1');
-            let eText = document.createElement('h2');
-            let error = response.status + ': ' + response.error;
-            console.log(error);
-            eTitle.innerHTML = 'Error: ' + response.status;
-            eText.innerHTML = response.error;
-            document.getElementsByClassName('content')[0].append(eTitle);
-            document.getElementsByClassName('content')[0].append(eText);
+    var sectionItems = [];
+    for (let item of shopItems) {
+        if (item.section.name == section) sectionItems.push(item);
+    }
 
-            return;
-        }
+    for (let i=0; i<sectionItems.length;i++) {
+        let item = sectionItems[i];
 
-        var shop_row = document.getElementById("item-shop-section");
-        while (shop_row.firstChild)
-        shop_row.removeChild(shop_row.firstChild);
+        makeCard(item);
+    }
+}
 
-        if (section === 'Daily')
-            section_data = response.data.daily;
-        else if (section === 'Featured')
-            section_data = response.data.featured;
-        else
-            section_data = response.data.specialFeatured;
+function makeCard(item) {
+    var shop_row = document.getElementById("item-shop-section");
 
-        for (let i = 0; i < section_data.entries.length; i++) {
-            const entry = section_data.entries[i];
-            if (entry.section.name === section) {
-                const items = section_data.entries[i].items;
-                const bundle  = section_data.entries[i].bundle;
-                
-                if (bundle !== null) {
-                    let b = document.createElement('div');
-                    b.classList.add('item-card-parent');
+    let parent = document.createElement('div');
+    parent.classList.add('item-card-parent');
 
-                    const item_obj = items[0];
+    var obj = document.createElement("div");
+    obj.classList.add("item-card");
+    obj.setAttribute('data-rarity', item.rarity.id.toLowerCase());
+    obj.setAttribute('data-type', item.mainType);
 
-                    var obj = document.createElement("div");
-                    obj.classList.add("item-card");
-                    obj.setAttribute('data-rarity', item_obj.rarity.value);
-                    
-                    var hold = document.createElement("div");
-                    hold.classList.add("item-info");
+    var hold = document.createElement("div");
+    hold.classList.add("item-info");
 
-                    var title = document.createElement('span');
-                    title.innerHTML = bundle.name;
-                    title.classList.add("item-title");
+    var title = document.createElement('a');
+    title.innerHTML = item.displayName;
+    title.classList.add("item-title");
 
-                    var price = document.createElement('a');
-                    var vbuck = document.createElement('img');
-                    vbuck.src = response.data.vbuckIcon;
-                    vbuck.classList.add("vbuck-icon");
+    var price = document.createElement('a');
+    var vbuck = document.createElement('img');
+    vbuck.src = 'https://media.fortniteapi.io/images/652b99f7863db4ba398c40c326ac15a9/transparent.png';
+    vbuck.classList.add("vbuck-icon");
 
-                    price.appendChild(vbuck);
-                    price.innerHTML += section_data.entries[i].finalPrice;
-                    price.classList.add('item-price');
+    let vbt = document.createElement('a');
+    vbt.classList.add("item-price-vbp");
 
-                    hold.appendChild(title);
-                    hold.appendChild(price);
+    let normalPrice = item.price.regularPrice;
+    let finalPrice = item.price.finalPrice;
 
-                    obj.appendChild(hold);
+    vbuck.title = finalPrice +'/'+ normalPrice;
 
-                    let ic = document.createElement('div');
-                    ic.classList.add("item-image");
-
-                    var img_obj = document.createElement("img");
-                    var img_src;
-
-                    img_obj.src = bundle.image;
-                    img_obj.setAttribute("title", bundle.name + ' for ' + section_data.entries[i].finalPrice + ' VBucks');
-                    ic.appendChild(img_obj);
-
-                    obj.appendChild(ic);
-
-                    b.append(obj);
-
-                    shop_row.append(b);
-                } else {
-                    for (let j = 0; j < items.length; j++) {
-                        let b = document.createElement('div');
-                        b.classList.add('item-card-parent');
-
-                        const item_obj = items[j];
-
-                        var obj = document.createElement("div");
-                        obj.classList.add("item-card");
-                        obj.setAttribute('data-rarity', item_obj.rarity.value);
-
-                        var hold = document.createElement("div");
-                        hold.classList.add("item-info");
+    if (typeof(normalPrice) !== "string" && typeof(finalPrice) !== "string") {
+        if (finalPrice < normalPrice) {
+            let bip = document.createElement('a');
+            bip.classList.add("item-price-bip");
     
-                        var title = document.createElement('span');
-                        title.innerHTML = item_obj.name;
-                        title.classList.add("item-title");
-    
-                        hold.appendChild(title);
-                        obj.appendChild(hold);
-
-                        var img_obj = document.createElement("img");
-                        var img_src;
-
-                        if (item_obj.images.featured != null)
-                            img_src = item_obj.images.featured;
-                        else if (item_obj.images.icon != null)
-                            img_src = item_obj.images.icon;
-                        else if (item_obj.images.smallIcon != null)
-                            img_src = item_obj.images.smallIcon;
-
-                        let ic = document.createElement('div');
-                        ic.classList.add("item-image");
-                        
-                        img_obj.src = img_src;
-                        img_obj.setAttribute("title", item_obj.name + ' for ' + section_data.entries[i].finalPrice + ' VBucks');
-                        ic.appendChild(img_obj);
-
-                        obj.append(ic);
-
-                        obj.addEventListener("click", function() {
-                            window.location.href = 'item.html?q=' + item_obj.name.toLowerCase();
-                        });
-
-                        b.append(obj);
-
-                        shop_row.append(b);
-                    }
-                }
-            }
-
-            for (let k = 0; k < shop_row.children.length; k++) {
-                let child = shop_row.children[k];
-                if (k >= 8) {
-                    shop_row.removeChild(child);
-                }
-            }
+            bip.innerHTML = normalPrice;
+            price.append(bip);
         }
-    })
+    }
+
+    vbt.innerHTML = finalPrice;
+
+    price.append(vbt);
+    
+    if (item.ignoreVbuck === undefined) {
+        price.appendChild(vbuck);   
+    }
+
+    price.classList.add('item-price');
+
+    hold.appendChild(title);
+
+    let type = document.createElement('a');
+    type.classList.add("item-type");
+    type.innerHTML = item.displayType;
+    hold.appendChild(type);
+
+    hold.appendChild(price);
+    hold.setAttribute('data-rarity', item.rarity.id.toLowerCase());
+
+    marqueeCheck(title);
+
+    obj.appendChild(hold);
+
+    let ic = document.createElement('div');
+    ic.classList.add("item-image");
+
+    let images = [];
+    let currentImage = 0;
+
+    for (let i = 0; i < item.displayAssets.length; i++) {
+        let displayAsset = item.displayAssets[i];
+
+        var img_obj = document.createElement("img");
+        var img_src;
+
+        if (displayAsset.url !== null)
+            img_src = displayAsset.url;
+        if (displayAsset.background !== null)
+            img_src = displayAsset.background;
+
+        img_obj.src = img_src;
+        img_obj.setAttribute("title", item.displayName + ' for ' + item.price.finalPrice + ' VBucks');
+        img_obj.setAttribute('otype', item.mainType);
+        img_obj.classList.add("style-picture");
+        
+        if (i === 0) img_obj.classList.add('current-style');
+
+        images.push(img_obj);
+        ic.appendChild(img_obj);
+    }
+    obj.append(ic);
+
+    if (item.displayAssets.length > 1) {
+        setInterval(function() {
+            currentImage ++;
+            if (currentImage > images.length - 1) currentImage = 0;
+
+            images[currentImage].classList.add('current-style');
+            for (let i = 0; i < images.length; i++) {
+                if (i !== currentImage) 
+                    images[i].classList.remove('current-style');
+            }
+        }, 5000);
+    }
+
+    if (item.banner !== null) {
+        let banner_object = document.createElement('i');
+        banner_object.classList.add('item-banner');
+        banner_object.innerHTML = item.banner.name;
+        banner_object.setAttribute('intensity', item.banner.intensity);
+        parent.appendChild(banner_object);
+    }
+
+    if (item.ignoreClicks === undefined) {
+        obj.addEventListener("click", function() {
+            window.location.href = 'item.html?q=' + item.displayName.toLowerCase();
+        });
+    }
+    if (item.href !== undefined) {
+        obj.addEventListener("click", function() {
+            window.location.href = item.href;
+        });
+    }
+
+    if (item.backgroundColors !== undefined) {
+        let colorStr = item.backgroundColors.join(',');
+
+        obj.style.background = 'radial-gradient(' + colorStr + ')';
+    }
+
+    parent.appendChild(obj);
+
+    shop_row.append(parent);
+}
+
+function newYearPayloads(christmas) {
+    let delay = christmas ? 300 : 600;
+    setInterval(function() {
+        const styleSheet = document.createElement('link');
+        styleSheet.rel = 'stylesheet';
+        styleSheet.href = 'css/snowflakes.css';
+        document.head.append(styleSheet)
+
+        // Imported from local education website. Undisclosed.
+        const snow = document.createElement("div");
+        snow.innerHTML = "<img src='assets/images/snowflake.svg'>";
+        snow.classList.add("snow");
+        snow.style.left = Math.random() * 100 + "vw";
+        snow.style.animationDuration = Math.random() * 5 + 8 + "s";
+        //document.getElementsByClassName('fn-dot-mask')[0].append(snow);
+        document.body.appendChild(snow);
+        setTimeout(() => { snow.remove(); }, 7000);
+    }, delay);
+}
+
+loadPayloads();
+function loadPayloads() {
+    let distances = [0, 0];
+    var now = new Date();
+    var lastYear = new Date(now.getFullYear() - 1, 11, 31);
+    var thisYear = new Date(now.getFullYear(), 11, 31);
+    distances[0] =  Math.floor(calcDays(now.getTime() - lastYear.getTime()))
+    distances[1] = Math.abs(Math.floor(calcDays(now.getTime() - thisYear.getTime())))
+    
+    if (distances[0] <= 10 || distances[1] <= 10) {
+        const christmas = sameDay(now, new Date(now.getFullYear(), 12, 25))
+        newYearPayloads(christmas);
+    }
 }
---
+++ js/vehicles.js
@@ -0,0 +1,36 @@
+function getstat() {
+    fetch(geturllang("https://fortniteapi.io/v2/game/vehicles", 1), {
+        headers: { 'Authorization': localStorage.keyFNAPIIo}
+    }).then(shop => shop.json()).then(data => {
+
+        let main = document.getElementById('vehicle-wrapper');
+
+        console.log(data);
+        for (let vehicle of data.vehicles) {
+            let parent = gne('div');
+            parent.classList.add('vehicle-item');
+
+            let image = document.createElement('img');
+            image.classList.add('vehicle-image');
+            image.src = vehicle.icon;
+            image.title = vehicle.name + ' (' + vehicle.id + ')';
+
+            let name = gne('p');
+            name.classList.add('vehicle-name');
+            name.textContent = vehicle.name;
+
+            let decoration = document.createElement('div');
+            decoration.classList.add('vehicle-decoration');
+
+            let bdec = gne('div');
+            bdec.classList.add('deco-b');
+            decoration.append(bdec);
+
+            parent.append(image, name, decoration);
+            main.appendChild(parent);
+        }
+
+    }).catch(err => {
+        console.error(err);
+    })
+}
