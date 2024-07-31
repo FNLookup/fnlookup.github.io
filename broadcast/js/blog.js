02515e99fe1bbfc6ca08e55c39986a2a69680dba
tposejank <81495861+tposejank@users.noreply.github.com>
2024-07-30 20:13:48 -0400

    broadcast updates

--- broadcast/js/blog.js
+++ broadcast/js/blog.js
@@ -1,5 +1,6 @@
 function loadBlog() {
-    fetch('https://raw.githubusercontent.com/FNLookup/fnlookup-blog/main/manifest.json')
+    let data = getApiRequestData('https://fnlookup-apiv2.vercel.app/api?broadcast=true');
+    fetch(data.url, data.data)
     .then(response => response.json())
     .then(json => {
         for (blog of json.directory) {
@@ -7,7 +8,7 @@ function loadBlog() {
                 let information = await fetch('https://raw.githubusercontent.com/FNLookup/fnlookup-blog/main/' + blog.id + '/info.json')
                 let infofile = await information.json()
 
-                document.getElementById('blog-entries').innerHTML += `            <a href="read/?id=${blog.id}" class="blog-post d-30-media">
+                document.getElementById('blog-entries').innerHTML += `            <a href="read/?id=${blog.id}" class="blog-post d-30-media fortnite-button-border">
                 <img src="https://raw.githubusercontent.com/FNLookup/fnlookup-blog/main/${blog.id}/${infofile['header-image']}" alt="Design" class="image">
                 <div class="information">
                     <h3 class="header-text-bold">${blog.shortTitle}</h3>
